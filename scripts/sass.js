95ec7b29948d1e313106c800d6763572e710f060
Yotam Shapira <yotams@elementor.com>
2022-07-17 21:51:02 +0300

    Upgrade node modules

--- scripts/sass.js
+++ scripts/sass.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
-const sass = require('node-sass');
+const sass = require('sass');
 
-const renderResult = sass.renderSync({
-    file: 'scss/elementor-icons.scss',
-});
+const renderResult = sass.compile('scss/elementor-icons.scss');
 
 const resultsDir = 'css';
 
